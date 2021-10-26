2c1e94557d814060c49f4dd9e1f719dcce2093fd
Michael MacFadden <michael@macfadden.org>
2021-10-25 22:28:09 -0500

    Cleaned up the test page javascript.

--- test-pages/test.js
+++ test-pages/test.js
@@ -5,8 +5,50 @@ const resultsTextArea = $("#results");
 const runButton = $("#run");
 const downloadButton = $("#download");
 
+let currentTest = null;
+
+const hooks = {
+  testStarted(module) {
+    currentTest = $(`<li class="list-group-item">Testing "${module}"...</li>`);
+    status.append(currentTest);
+    scrollToBottomOfStatus();
+  },
+  testFinished(module) {
+    currentTest.append(" Done");
+  }
+}
+
+async function loadTest() {
+  status.empty();
+  resultsTextArea.val("");
+
+  runButton.prop("disabled", true);
+  downloadButton.prop("disabled", true);
+
+  try {
+    const results = await LoadTester.testAll("password", localStorage, true, hooks);
+    currentTest = $(`<li class="list-group-item">All tests complete.</li>`);
+    scrollToBottomOfStatus();
+    const data = results.join("\n");
+    resultsTextArea.val(data);
+
+    runButton.prop("disabled", false);
+    downloadButton.prop("disabled", false);
+  } catch (e) {
+    runButton.prop("disabled", false);
+    downloadButton.prop("disabled", true);
+    status.append($(`<div>Error (see console for more details): ${e.message}</div>`));
+    console.log(e);
+  }
+}
+
+function scrollToBottomOfStatus() {
+  status.scrollTop(status[0].scrollHeight);
+}
+
+
 function downloadCsv() {
-  const textFileAsBlob = new Blob([resultsTextArea.val()], {type:'text/plain'});
+  const textFileAsBlob = new Blob([resultsTextArea.val()], {type: 'text/plain'});
   const downloadLink = document.createElement("a");
   downloadLink.download = "load-test-results.csv";
   downloadLink.innerHTML = "Download File";
@@ -28,46 +70,4 @@ function downloadCsv() {
   if (downloadLink.parentNode) {
     downloadLink.parentNode.removeChild(downloadLink);
   }
-}
-
-
-let currentTest = null;
-
-const hooks = {
-  testStarted(module) {
-    currentTest = $(`<li class="list-group-item">Testing "${module}"...</li>`);
-    status.append(currentTest);
-    scrollToBottomOfStatus();
-  },
-  testFinished(module) {
-    currentTest.append(" Done");
-  }
-}
-
-async function loadTest() {
-  status.empty();
-  resultsTextArea.val("");
-
-  runButton.prop("disabled",true);
-  downloadButton.prop("disabled",true);
-
-  try {
-    const results = await LoadTester.testAll("password", localStorage, true, hooks);
-    currentTest = $(`<li class="list-group-item">All tests complete.</li>`);
-    scrollToBottomOfStatus();
-    const data = results.join("\n");
-    resultsTextArea.val(data);
-
-    runButton.prop("disabled",false);
-    downloadButton.prop("disabled",false);
-  } catch (e) {
-    runButton.prop("disabled",false);
-    downloadButton.prop("disabled",true);
-    status.append($(`<div>Error (see console for more details): ${e.message}</div>`));
-    console.log(e);
-  }
-}
-
-function scrollToBottomOfStatus() {
-  status.scrollTop(status[0].scrollHeight);
 }
